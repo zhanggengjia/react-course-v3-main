fcce96b4c1615d507c9d7985f00c6d7c7e97c3bf
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-07-06 01:44:44 +0800

    strapi-2025/7/6

---
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et necessitatibus pariatur officia recusandae officiis dolorum. Exercitationem nulla tempore est, perferendis itaque, eos fuga repudiandae sunt temporibus aperiam, deleniti natus ducimus.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
