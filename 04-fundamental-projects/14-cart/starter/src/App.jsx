8546362a5cf449afeb601d482e32f401dd448fcb
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-07-06 20:08:23 +0800

    cart-2025/7/6

--- 04-fundamental-projects/14-cart/starter/src/App.jsx
+++ 04-fundamental-projects/14-cart/starter/src/App.jsx
@@ -1,8 +1,18 @@
 // components
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
+import { useGlobalContext } from './context';
 
 function App() {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <main style={{ display: 'grid', placeItems: 'center', width: '100vw', height: '100vh' }}>
+        <div className='loading'></div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Navbar />
--- 04-fundamental-projects/14-cart/starter/src/CartContainer.jsx
+++ 04-fundamental-projects/14-cart/starter/src/CartContainer.jsx
@@ -1,7 +1,9 @@
 import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 import cartItems from './data';
 const CartContainer = () => {
-  const cartArray = [...cartItems];
+  const { cart, clearCart, totalCost } = useGlobalContext();
+  const cartArray = Array.from(cart.entries());
 
   if (cartArray.length === 0) {
     return (
@@ -23,7 +25,8 @@ const CartContainer = () => {
       {/* cart items */}
       <div>
         {cartArray.map((cartItem) => {
-          return <CartItem key={cartItem.id} {...cartItem} />;
+          const [id, item] = cartItem;
+          return <CartItem key={id} {...item} />;
         })}
       </div>
       {/* cart footer */}
@@ -31,12 +34,12 @@ const CartContainer = () => {
         <hr />
         <div>
           <h5 className='cart-total'>
-            total <span>$10</span>
+            total <span>${totalCost.toFixed(2)}</span>
           </h5>
         </div>
         <button
           className='btn btn-hipster'
-          onClick={() => console.log('clear cart')}
+          onClick={clearCart}
         >
           clear cart
         </button>
