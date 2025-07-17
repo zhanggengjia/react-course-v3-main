2768760e01e955ff34467c8f8053ad1c9a6d787d
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-07-18 00:44:01 +0800

    unsplash-2025/7/18

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
+import { useGlobalContext } from "./context";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const url = `https://api.unsplash.com/search/photos?client_id=${apiKey}`;
+console.log(apiKey)
 
 const Gallery = () => {
-  const { } = useGlobalContext();
+  const { searchTerm } = useGlobalContext();
+  const response = useQuery({
+    queryKey: ['images', searchTerm],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${searchTerm}`);
+
+      return result.data;
+    }
+  });
+
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+
+  if (response.isError) {
+    retrun(
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    )
+  }
+
+  const results = response.data.results;
+  if (results.length < 1) {
+    retrun(
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
 
   return (
-    <div>Gallery</div>
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img src={url} key={item.id} alt={item.alt_description} className="img"></img>
+        );
+      })}
+    </section>
   )
 }
 
--- 07-unsplash-images/starter/src/context.jsx
+++ 07-unsplash-images/starter/src/context.jsx
@@ -2,18 +2,48 @@ import { createContext, useContext, useState, useEffect, Children } from "react"
 
 const AppContext = createContext();
 
+// const getInitialDarkMode = () => {
+//   const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark').matches;
+//   const storeDarkMode = localStorage.getItem('darkTheme');
+
+//   if (storeDarkMode) {
+//     if (storeDarkMode === 'true') {
+//       return true;
+//     } else {
+//       return false
+//     }
+//   } else if (prefersDarkMode === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+const getInitialDarkMode = () => {
+  const raw = localStorage.getItem('darkTheme');
+  if (raw === 'true') return true;
+  if (raw === 'false') return false;
+
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return prefersDarkMode;
+};
+
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode);
+  const [searchTerm, setSearchTerm] = useState('cat');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme)
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', newDarkTheme);
-    console.log(body)
+    localStorage.setItem('darkTheme', newDarkTheme);
   }
 
-  return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>{children}</AppContext.Provider>
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
+
+  return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}>{children}</AppContext.Provider>
 }
 
 export const useGlobalContext = () => { return useContext(AppContext) }
