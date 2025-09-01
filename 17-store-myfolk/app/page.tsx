92e7d916a91b72b7377ea322951e45eabe92cc7e
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-09-01 18:26:50 +0800

    store-2025/9/1

---
+++ 17-store-myfolk/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
+}
+
+export default HomePage;
