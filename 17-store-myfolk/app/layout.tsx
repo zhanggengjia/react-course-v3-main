f7cb4afaeb24a0e040a3aa2ad9a1eee5486f4269
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-09-04 19:46:49 +0800

    store-2025/9/4

--- 17-store-myfolk/app/layout.tsx
+++ 17-store-myfolk/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="overflow-y-scroll">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
--- 17-store-myfolk/app/products/page.tsx
+++ 17-store-myfolk/app/products/page.tsx
@@ -1,5 +1,15 @@
-function ProductsPage() {
-  return <div>ProductsPage</div>;
+import ProductsContainer from '@/components/products/ProductsContainer';
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) {
+  const params = await searchParams;
+  const layout = params.layout || 'grid';
+  const search = params.search || '';
+  console.log(params);
+  return <ProductsContainer layout={layout} search={search} />;
 }
 
 export default ProductsPage;
